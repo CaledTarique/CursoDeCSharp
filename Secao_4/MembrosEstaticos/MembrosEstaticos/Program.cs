d2dfbc3a6dec7b8e6828dc079989d5632911cf99
Cáled Tarique Pereira <caled@adven.tech>
2023-05-19 08:36:32 -0300

    Conteúdo e exercícios da seção 4 do Curso de C#

---
+++ Secao_4/MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace MembrosEstaticos
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            Console.WriteLine("Digite o valor do raio:");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine($"Circuferencia: {circ}");
+            Console.WriteLine($"Volume: {volume}");
+
+        }
+    }
+}
---
+++ Secao_4/MembrosEstaticos_2/MembrosEstaticos_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace MembrosEstaticos_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do raio:");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circuferencia: {circ}");
+            Console.WriteLine($"Volume: {volume}");
+        }
+    }
+}
