595f32bce3dbcf7b86f993ffa2c525ab1237ff4a
Cáled Tarique Pereira <caled@adven.tech>
2023-05-23 22:47:28 -0300

    Conteúdo e exercícios da seção 4 do Curso de C#

---
+++ Secao_4/ExerciciosDeFixacaoMembrosEstaticoa/ExerciciosDeFixacaoMembrosEstaticoa/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosDeFixacaoMembrosEstaticoa
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double DolarParaReal(double cotacaoDolar, double quantia)
+        {
+            quantia = (quantia * cotacaoDolar) * (1 + IOF/100) ;
+            return quantia;
+        }
+    }
+}
---
+++ Secao_4/ExerciciosDeFixacaoMembrosEstaticoa/ExerciciosDeFixacaoMembrosEstaticoa/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+namespace ExerciciosDeFixacaoMembrosEstaticoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dolar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolaresComprados = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double quantia = ConversorDeMoeda.DolarParaReal(cotacaoDolar, dolaresComprados);
+
+            Console.WriteLine($"Valor a ser pago em reais: {quantia}");
+        }
+    }
+}
