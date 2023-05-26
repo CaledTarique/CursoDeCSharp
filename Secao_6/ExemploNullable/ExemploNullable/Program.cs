1ac5174b7652790f88b65160ae29cf4b565057fd
Cáled Tarique Pereira <caled@adven.tech>
2023-05-25 22:37:11 -0300

    Conteúdo e Exercícios da Seção 6 do Curso de C#

---
+++ Secao_6/ExemploNullable/ExemploNullable/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ExemploNullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Forma de se atribuir valor null a variavel do tipo valor
+
+            Nullable<double> x = null;
+            //ou
+            double? y = null;
+            double? z = 10.0;
+
+            Console.WriteLine("GetValueOrDefault:");
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+            Console.WriteLine(z.GetValueOrDefault());
+            Console.WriteLine();
+
+            Console.WriteLine("HasValue:");
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+            Console.WriteLine(z.HasValue);
+            Console.WriteLine();
+
+            Console.WriteLine("Value:");
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            if (z.HasValue)
+                Console.WriteLine(z.Value);
+            Console.WriteLine();
+
+            Console.WriteLine("Operador de coalescência nula:");
+
+            double w = y ?? 5.0;
+            Console.WriteLine(w);
+        }
+    }
+}
