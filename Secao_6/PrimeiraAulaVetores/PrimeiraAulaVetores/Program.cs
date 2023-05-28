275a10c61504bfbed4d18d22d90af222ecf72c75
Cáled Tarique Pereira <caled@adven.tech>
2023-05-28 17:11:15 -0300

    Conteúdo e Exercícios da Seção 6 do Curso de C#

---
+++ Secao_6/PrimeiraAulaVetores/PrimeiraAulaVetores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace PrimeiraAulaVetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+            double soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                soma +=  vect[i];
+            }
+
+            double media = soma / n;
+            Console.WriteLine($"A altura média é: {media.ToString("F2")}");
+        }
+    }
+}
---
+++ Secao_6/SegundaAulaVetores/SegundaAulaVetores/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SegundaAulaVetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Product[] vect = new Product[n];
+            double soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+
+                vect[i] = new Product { Name = name, Price = price };
+                soma += vect[i].Price;
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine($"A media de preço dos produtos é: {media}");
+        }
+    }
+}
